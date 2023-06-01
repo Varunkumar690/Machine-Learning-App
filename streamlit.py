1e7d8b1bbdc8e341a52de94c352cf1dd0ff97c91
Varun Kumar <112616176+Varunkumar690@users.noreply.github.com>
2023-06-01 11:00:04 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,20 +12,6 @@ from streamlit_pandas_profiling import st_profile_report
 import pycaret
 from pycaret.classification import setup,compare_models,pull,save_model,ClassificationExperiment
 from pycaret.regression import setup,compare_models,pull,save_model,RegressionExperiment
-st.title('🎈 pydataset')
-
-selected_data = st.sidebar.selectbox('Select a dataset', data().dataset_id)
-title_data = data()[ data()['dataset_id'] == selected_data]['title']
-
-st.header('Datasets')
-st.subheader('List of dataset')
-with st.expander('Show list of dataset'):
-    st.write(data())
-
-    st.subheader(f'Selected data (`{selected_data}`)')
-    st.info(title_data)
-    st.write(data(selected_data))
-
 
 st.title("Machine Learning App using pycaret")
 
